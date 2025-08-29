1860853edb9f5403be479c12f796c706c7df9224
João Guilherme Santana de Oliveira <joaogsantanadeoliveira@gmail.com>
2025-08-29 13:16:22 -0300

    Update ex02.py
    Adicionei o título do exercício!

--- ex02.py
+++ ex02.py
@@ -1,2 +1,3 @@
+# Exercício Python 02: Crie um script Python que leia o nome de uma pessoa e mostre uma mensagem de boas-vindas. Obs: esse é sem o .format
 nome = input('Qual o seu nome? ')
 print('É um grande prazer em te conhecer',nome)
