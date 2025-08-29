d506352728dcbd3eccdd0c5148a53d978134f009
João Guilherme Santana de Oliveira <joaogsantanadeoliveira@gmail.com>
2025-08-29 13:13:43 -0300

    Update ex05.py
    Refazendo o exercício de uma forma melhor.

--- ex05.py
+++ ex05.py
@@ -1,4 +1,11 @@
 # Exercício Python 05: Crie um programa que leia algo pelo teclado e mostre na tela o seu tipo primitivo e todas as informações possíveis sobre ele.
-n = input('Digite um algo:')
-print('O tipo primitivo desse valor é', type(n))
-print(n.isupper(), n.isnumeric(), n.isalnum())
+a = input('Digite um algo:')
+print('O tipo primitivo desse valor é', type(a))
+print('Só tem espaços? {}'.format(a.isspace()))
+print('É um número? {}' .format(a.isnumeric()))
+print('É alfabético? {}'.format(a.isalpha()))
+print('É alfanumérico? {}'.format(a.isalnum()))
+print('Está em letras maiúsculas? {}'.format(a.isupper()))
+print('Está em letras minúsculas? {}'.format(a.islower()))
+print('Está capitalizada? {}'.format(a.istitle()))
+
