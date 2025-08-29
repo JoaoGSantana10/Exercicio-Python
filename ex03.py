be57dcb5ff88fd1c338d620849faf2fcd1142b88
João Guilherme Santana de Oliveira <joaogsantanadeoliveira@gmail.com>
2025-08-29 13:16:33 -0300

    Update ex03.py
    Adicionei o título do exercício!

--- ex03.py
+++ ex03.py
@@ -1,2 +1,3 @@
+# Exercício Python 03: Crie um programa que leia o nome de uma pessoa e mostre uma mensagem de boas-vindas.
 nome = input('Digite seu nome: ')
 print('É um prazer em te conhecer, {}!'.format(nome))
